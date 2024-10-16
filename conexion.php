6f5ee293cf5bb54e64318e92c2a864237d9c68db
samaidy-25 <samquintero7@ielasnieves.edu.co>
2024-10-16 16:26:14 -0500

    ejercicio finalizado

---
+++ conexion.php
@@ -0,0 +1,15 @@
+<?php
+// Conexión a la base de datos
+$servidor = "localhost";
+$usuario = "root";
+$contrasena = "";
+$basedatos = "web";
+
+
+$conexion = new mysqli($servidor, $usuario, $contrasena, $basedatos);
+
+
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+    } else {echo"Base de datos conectada";}
+?>
